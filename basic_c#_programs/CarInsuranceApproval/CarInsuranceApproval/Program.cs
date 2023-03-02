81d8fcc7c21b7e056776e744feb9cccb06f010c3
greg <grandersson1991@gmail.com>
2023-03-02 16:08:16 -0500

    Car Insurance Approval proj added
    submission for step 142

---
+++ basic_c#_programs/CarInsuranceApproval/CarInsuranceApproval/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInsuranceApproval
+{
+    class Program
+    {
+        static void Main()
+        {
+            // questions for input
+            Console.WriteLine("What is your age?");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? (true/false)");
+            bool hasDUI = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int totalTickets = Convert.ToInt32(Console.ReadLine());
+
+            // bool logic here
+            bool isQualified = (userAge > 15 && !hasDUI && totalTickets <= 3);
+
+            // print results
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(isQualified);
+            Console.ReadLine();
+        }
+    }
+}
