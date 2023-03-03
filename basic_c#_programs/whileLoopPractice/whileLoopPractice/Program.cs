cdc68998acc6fc5c05ea4853f9e17fa1829f3689
greg <grandersson1991@gmail.com>
2023-03-02 19:50:07 -0500

    loop practice submission
    step 171 in c#/unity course

---
+++ basic_c#_programs/whileLoopPractice/whileLoopPractice/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace whileLoopPractice
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            string userGuess = "";
+
+            while (userGuess.ToLower() != "tiger")
+            {
+                Console.WriteLine("Please guess an animal:");
+                userGuess = Console.ReadLine();
+            }
+            Console.WriteLine("\"tiger\" is correct! grrrr");
+            Console.WriteLine("Press any button followed by enter to continue.");
+            _ = Console.ReadLine();
+
+            do
+            {
+                Console.WriteLine("Please guess an animal:");
+                userGuess = Console.ReadLine();
+            }
+            while (userGuess != "bear");
+            Console.WriteLine("\"bear\" is correct! rawrrrrr");
+            Console.WriteLine(" -- program end");
+            _ = Console.ReadLine();
+        }
+    }
+}
