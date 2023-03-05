65db71ee7dd04644cbe384b35fb03c5028d3f12a
greg <grandersson1991@gmail.com>
2023-03-05 11:34:37 -0500

    C# submissions, mainly ConsoleAppProject
    step 196 submission for console app assignment.

---
+++ basic_c#_programs/ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] words = { "the", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog" };
+            Console.WriteLine("please type your favorite 4-letter word");
+            string favWord = Console.ReadLine();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i] + favWord;
+            }
+            for (int j = 0; j < words.Length; j++)
+            {
+                Console.WriteLine(words[j]);
+            }
+            Console.WriteLine("Press enter to continue.");
+            _ = Console.ReadLine();
+
+            //comment below was infinite loop
+            //for (int a = 0; a > -1; a++)
+            for (int a = 0; a < 10; a++)
+            {
+                //if (a % 10000 == 0) {
+                Console.WriteLine("wooooohooooooo");
+                //}
+            }
+            Console.WriteLine("Press enter to continue.");
+            _ = Console.ReadLine();
+
+            // while loop with < and <= operators
+            int targetNum = 42;
+            int counter = 0;
+            while (counter < targetNum)
+            {
+                Console.WriteLine(counter);
+                counter++;
+            }
+            Console.WriteLine("Press enter to continue.");
+            _ = Console.ReadLine();
+            // now counter must pass target
+            int targetNum2 = 42;
+            int counter2 = 0;
+            while (counter2 <= targetNum2)
+            {
+                Console.WriteLine(counter2);
+                counter2++;
+            }
+            Console.WriteLine("Press enter to continue.");
+            _ = Console.ReadLine();
+
+            // step 194 -- lists
+            List<string> uniqueWords = new List<string>() { "blizzard", "flood", "avalanche", "earthquake", "tsunami", "hurricane", "tornado", "drought", "landslide" };
+            Console.WriteLine("Enter a type of natural disaster. We'll let you know if it's included in the official list.");
+            string userChoice = Console.ReadLine().ToLower();
+            // easier way using method
+            //int choiceIndex = uniqueWords.IndexOf(userChoice);
+
+            // loop for IndexOf
+            int choiceIndex = -1;
+            for (int i = 0; i < uniqueWords.Count; i++)
+            {
+                if (uniqueWords[i] == userChoice)
+                {
+                    choiceIndex = i;
+                    break;
+                }
+            }
+            // is the user's input in the list ?
+            if (choiceIndex == -1)
+            {
+                Console.WriteLine("Unfortunately, that choice is not listed on the official natural disasters page.");
+            } else
+            {
+                Console.WriteLine("Your choice of " + userChoice + " is in index " + choiceIndex + " of the list.");
+            }
+            Console.WriteLine("Press enter to continue.");
+            _ = Console.ReadLine();
+
+            // step 195 - lists but without uniqueness. find all indices of an input
+            List<string> nonUniqueWords = new List<string>() { "blizzard", "flood", "blizzard", "avalanche", "earthquake", "earthquake", "tsunami", "earthquake", "hurricane", "earthquake", "tornado", "tornado", "drought", "landslide" };
+            Console.WriteLine("Enter a type of natural disaster. We'll let you know where it's included in the list of recent disasters.");
+            string userChoice2 = Console.ReadLine().ToLower();
+            Console.WriteLine("Your choice of " + userChoice2 + " is listed at the following indices:");
+
+            // loop for indices
+            List<int> inputIndices = new List<int>();
+            for (int i = 0; i < nonUniqueWords.Count; i++)
+            {
+                if (nonUniqueWords[i] == userChoice2)
+                {
+                    inputIndices.Add(i);
+                    Console.WriteLine(i);
+                }
+            }
+            // is the user's input in the list ?
+            if (inputIndices.Count == 0)
+            {
+                Console.WriteLine("Unfortunately, that choice is not listed on the official natural disasters page.");
+            }
+            // other way but uses double loop
+            //Console.WriteLine("Your choice of " + userChoice2 + " is in the following indices:");
+            //for (int j = 0; j < inputIndices.Count; j++)
+            //{
+            //    Console.WriteLine(inputIndices[j]);
+            //}
+            Console.WriteLine("Press enter to continue.");
+            _ = Console.ReadLine();
+
+            // step 196 - foreach loop
+            List<string> randomWords = new List<string>() { "in", "general", "a", "general", "has", "the", "role", "of", "a", "leader" };
+            List<string> alreadyUsed = new List<string>();
+            foreach (string word in randomWords)
+            {
+                Console.WriteLine(word);
+                if (alreadyUsed.Contains(word) == true)
+                {
+                    Console.WriteLine("This word has already appeared in the list.");
+                } else
+                {
+                    Console.WriteLine("This is the first occurence of this word.");
+                    alreadyUsed.Add(word);
+                }
+            }
+            Console.WriteLine("Press enter to exit the program.");
+            _ = Console.ReadLine();
+        }
+    }
+}
---
+++ basic_c#_programs/IterationProject/IterationProject/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IterationProject
+{
+    class Program
+    {
+        static void Main()
+        {
+            //int[] testScores = { 98, 99, 85, 70, 83, 34, 91, 90, 94 };
+
+            //for (int i = 0; i < testScores.Length; i++ )
+            //{
+            //    if (testScores[i] > 85)
+            //    {
+            //        Console.WriteLine("Passing test score: " + testScores[i]);
+            //    }
+            //}
+            //Console.ReadLine();
+
+            //string[] names = { "Greg", "Phyllis", "Adam", "Daniel", "Rick" };
+
+            //for (int j = 0; j < names.Length; j++)
+            //{
+            //    if (names[j].Length == 4)
+            //    {
+            //        Console.WriteLine(names[j]);
+            //    }
+            //}
+            //_ = Console.ReadLine();
+
+            //List<int> testScores = new List<int>();
+            //testScores.Add(98);
+            //testScores.Add(99);
+            //testScores.Add(81);
+            //testScores.Add(72);
+            //testScores.Add(70);
+
+            //foreach (int score in testScores)
+            //{
+            //    if (score > 85)
+            //    {
+            //        Console.WriteLine("passing test score: " + score);
+            //    }
+            //}
+
+            //List<string> names = new List<string>() { "Greg", "Phyllis", "Adam", "Daniel" };
+
+            //foreach (string name in names)
+            //{
+            //    if (name == "Greg")
+            //    {
+            //        Console.WriteLine(name);
+            //    }
+            //}
+            //Console.ReadLine();
+
+            List<int> testScores = new List<int>() {  98, 99, 12, 74, 23, 99 };
+            List<int> passingScores = new List<int>();
+
+            foreach (int score in testScores)
+            {
+                if (score > 85)
+                {
+                    passingScores.Add(score);
+                }
+            }
+            Console.WriteLine(passingScores.Count);
+            _ = Console.ReadLine();
+        }
+    }
+}
