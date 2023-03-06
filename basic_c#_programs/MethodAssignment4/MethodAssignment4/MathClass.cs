c1463311dc160f64fa0414e0d5158ed7ca8f0157
greg <grandersson1991@gmail.com>
2023-03-06 17:23:25 -0500

    Step 220 Methods Submission
    last of the method sections submissions

---
+++ basic_c#_programs/MethodAssignment4/MethodAssignment4/MathClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment4
+{
+    class MathClass
+    {
+        public static void Method1(int x, int y)
+        {
+            x += 42;
+            Console.WriteLine("Second parameter: {0}", y);
+        }
+    }
+}
---
+++ basic_c#_programs/MethodAssignment4/MethodAssignment4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment4
+{
+    class Program
+    {
+        static void Main()
+        {
+            MathClass.Method1(x: 19, y: 42);
+            _ = Console.ReadLine();
+        }
+    }
+}
