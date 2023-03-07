3f03ff071803c57d5dc931a03e2dbca9f96d6cf3
greg <grandersson1991@gmail.com>
2023-03-07 10:17:29 -0500

    Step 256 Parameters Assignment
    submission for step 256

--- basic_c#_programs/AbstractClassAssignment/AbstractClassAssignment/Employee.cs
+++ basic_c#_programs/AbstractClassAssignment/AbstractClassAssignment/Employee.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace AbstractClassAssignment
 {
-    public class Employee: Person, IQuittable
+    public class Employee<T>: Person, IQuittable
     {
         public int Id { get; set; }
+        public List<T> Things { get; set; }
         public override void SayName()
         {
             Console.WriteLine("Our next new employees name is: {0} {1}", FirstName, LastName);
@@ -13,27 +18,30 @@ namespace AbstractClassAssignment
         {
             Console.WriteLine("Breaking News! Employee {0} {1} has just gotten up and walked out of the building!", FirstName, LastName);
         }
-        public static bool operator== (Employee emp1, Employee emp2)
-        {
-            if (emp1.Id == emp2.Id)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        public static bool operator!= (Employee emp1, Employee emp2)
-        {
-            if (emp1.Id != emp2.Id)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
+
+        // this is needed for a previous step, but breaks when changing employee to generic
+
+        //public static bool operator== (Employee emp1, Employee emp2)
+        //{
+        //    if (emp1.Id == emp2.Id)
+        //    {
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+        //}
+        //public static bool operator!= (Employee emp1, Employee emp2)
+        //{
+        //    if (emp1.Id != emp2.Id)
+        //    {
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+        //}
     }
 }
